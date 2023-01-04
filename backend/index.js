e8cb02bb7cfa87c028d61e591ab1117e480017e7
Sumit <sumit.123sahni@gmail.com>
2023-01-04 22:26:05 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express =  require("express");
+const path = require("path");
+const PORT = process.env.port || 5000;
+const app = express();
+
+// app.use(express.static(path.join(__dirname,"./client/build")));
+
+// console.log(__dirname)
+
+// app.use("*", function(_, res){
+//      res.sendFile(path.join(__dirname,"./client/build/index.html")),
+//      function(err) {
+//         res.status(500).send(err.message)
+//      }
+// });
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`);
+})
